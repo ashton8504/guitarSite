d198c1d43bdc7c445f6429b3921f7f285ed51150
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-12 13:15:57 -0400

    updated

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const footer = document.querySelector(".jerryFooter");
 
-window.addEventListener("scroll", () => {
+function updateScrollableHeight() {
   const scrollableHeight =
     document.documentElement.scrollHeight - window.innerHeight;
+  return scrollableHeight;
+}
+
+window.addEventListener("scroll", () => {
+  const scrollableHeight = updateScrollableHeight();
   if (window.scrollY >= scrollableHeight) {
     footer.classList.add("show-footer");
   } else {
     footer.classList.remove("show-footer");
   }
 });
+
+window.addEventListener("load", () => {
+  const scrollableHeight = updateScrollableHeight();
+});
+
+window.addEventListener("resize", () => {
+  const scrollableHeight = updateScrollableHeight();
+});
+
+// const footer = document.querySelector(".jerryFooter");
+// const imageContainer = document.querySelector(".image-container");
+
+// window.addEventListener("scroll", () => {
+//   const scrollableHeight = imageContainer.offsetHeight - window.innerHeight;
+//   if (window.scrollY >= scrollableHeight) {
+//     footer.classList.add("show-footer");
+//   } else {
+//     footer.classList.remove("show-footer");
+//   }
+// });
