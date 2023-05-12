5219bdffcafa2da0ff48ab6ed797dc23166aa511
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-12 13:20:59 -0400

    Updated
    Added new photos and created a JS feature where if you click on logo it goes to the top

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const footer = document.querySelector(".jerryFooter");
+const logo = document.querySelector(".company-logo-jerry-page");
+
+logo.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 function updateScrollableHeight() {
   const scrollableHeight =
@@ -22,15 +27,3 @@ window.addEventListener("load", () => {
 window.addEventListener("resize", () => {
   const scrollableHeight = updateScrollableHeight();
 });
-
-// const footer = document.querySelector(".jerryFooter");
-// const imageContainer = document.querySelector(".image-container");
-
-// window.addEventListener("scroll", () => {
-//   const scrollableHeight = imageContainer.offsetHeight - window.innerHeight;
-//   if (window.scrollY >= scrollableHeight) {
-//     footer.classList.add("show-footer");
-//   } else {
-//     footer.classList.remove("show-footer");
-//   }
-// });
